41cdc6062c35ff81061404cd47a9911647ad6abc
stuck <stuck@git.chicchi7393.xyz>
2023-07-06 14:33:01 +0200

    Add caching support to avoid too many api calls

--- apodutils.js
+++ apodutils.js
@@ -4,15 +4,30 @@ var fetch = require("node-fetch");
 
 const API_KEY = fs.readFileSync('API_KEY', { encoding: 'utf-8', flag: 'r' });
 const APOD_URL = `https://api.nasa.gov/planetary/apod`
+const ARCHIVE_DIR = __dirname + '/archive/'
 
 function formatDate(date) {
   return new Date(date).toISOString().split('T')[0]
 }
 
+function cacheAPOD(apod) {
+	apodString = JSON.stringify(apod, null, 2)
+	filename = ARCHIVE_DIR + apod.date
+	fs.writeFileSync(filename, apodString)
+}
+
 async function getAPOD(date=new Date()) {
   let dateISO = formatDate(date)
-  const res = await fetch(`${APOD_URL}?api_key=${API_KEY}&date=${dateISO}`);
-  const apod = await res.json();
+  let apod = {}
+  if (fs.existsSync(ARCHIVE_DIR + date)) {
+  	let apodString = fs.readFileSync(ARCHIVE_DIR + date,
+  									{ encoding: 'utf-8', flag: 'r' })
+  	apod = JSON.parse(apodString)
+  } else {
+  	let res = await fetch(`${APOD_URL}?api_key=${API_KEY}&date=${dateISO}`);
+	apod = await res.json();
+	cacheAPOD(apod);
+  }
   return apod
 }
 
